9b1be500700a1f7edfc91f585a397e25ee0bf0ee
vutukuridivyareddy <137431020+vutukuridivyareddy@users.noreply.github.com>
2024-04-08 18:06:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws-instance" "instance1" {
+resource "aws-instance" "ec21" {
   ami = "ami-007020fd9c84e18c7"
   instance_type = "t2.micro"
   key_name = "key-divya"
+  security_groups = ["launch-wizard-9"]
   instance_tenancy = "default" 
   tags = {
     name = "terraform instance"
-  }
-  
+  } 
 }
