2c80fbc7cc10096354ebb2378274f7f57f253aab
vutukuridivyareddy <137431020+vutukuridivyareddy@users.noreply.github.com>
2024-04-08 16:49:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "instance1" {
   ami = "ami-007020fd9c84e18c7"
   instance_type = "t2.micro"
-  key_name = "key-divya"
-  instance_tenancy = "default" 
+  key_name = "key-divya" 
   tags = {
     name = "terraforminstance"
   }
