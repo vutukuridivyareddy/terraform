c5e934ce91de0b01e63f5d8f75324c19efee0201
vutukuridivyareddy <137431020+vutukuridivyareddy@users.noreply.github.com>
2024-04-08 17:40:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-resource "aws_instance" "instance1" {
-  ami = "ami-007020fd9c84e18c7"
-  instance_type = "t2.micro"
-  key_name = "key-divya" 
-  tags = {
-    name = "terraforminstance"
-  }
-  
+resource "aws_instance" "ec2" {
+    ami = "ami-007020fd9c84e18c7"
+    instance_type = "t2.micro"
+    key_name = "test"
+    tags = {
+        Name = "terraform-ec2-1"
+    }
 }
