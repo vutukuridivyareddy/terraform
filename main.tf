a566fdf62b2fb8fc4892237bbd11a6f77eec6fcc
vutukuridivyareddy <137431020+vutukuridivyareddy@users.noreply.github.com>
2024-04-11 12:28:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec2" {
     ami = "ami-007020fd9c84e18c7"
     instance_type = "t2.micro"
+    security_groups = ["launch-wizard-9"]
     key_name = "test"
     tags = {
         Name = "terraform-ec2-1"
